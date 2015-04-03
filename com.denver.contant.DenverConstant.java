2714d70cd6538bb035f45b25f770678eddf15297
sacdpathade <sacdpathade@gmail.com>
2015-04-03 05:59:40 +0530

    Create com.denver.contant.DenverConstant.java
    Constant file

---
+++ com.denver.contant.DenverConstant.java
@@ -0,0 +1,32 @@
+package com.denver.contant;
+
+/**
+ * Constants file for Denver bag conveyor system.
+ * @author SACHIN PATHADE
+ *
+ */
+public class DenverConstant {
+	public static enum BAG_INFO {
+		BAG_NUMBER, ENTRY_POINT, FLIGHT_ID
+	}
+
+	public static enum CONVEYOR_INFO {
+		NODE1, NODE2, TRAVEL_TIME
+	}
+
+	public static enum DEPARTURE_INFO {
+		FLIGHT_ID, FLIGHT_GATE, DESTINATION , FLIGHT_TIME
+	}
+
+	public static final String  ARRIVAL="ARRIVAL";
+	public static final String  BAGGAGECLAIM = "BaggageClaim";
+
+
+	public static final String  BAGS="Bags";
+	public static final String  CONVEYOR_SYSTEM ="Conveyor System";
+
+	public static final String  DEPARTURES ="Departures";
+	public static final String  SECTION="# Section";
+
+
+}
